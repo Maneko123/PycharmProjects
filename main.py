836c365a821324443cbfc8637117805dd82142ad
Ivan <ivangrekov83@gmail.com>
2023-11-14 21:50:05 +0300

    Yes

---
+++ main.py
@@ -0,0 +1,39 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt5.QtGui import QPainter, QColor
+from random import randint
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(0, 0, 5000, 2400)
+        self.setWindowTitle('Pushbutton')
+        self.button = QPushButton('Нажми',self)
+        self.button.clicked.connect(self.draw)
+
+    def draw(self):
+        self.update()
+
+    def paintEvent(self, event):
+        qp = QPainter()
+        qp.begin(self)
+        color = QColor(255, 255, 0)
+        qp.setBrush(color)
+        qp.setPen(color)
+        size = randint(10, 300)
+        x = randint(0, 400)
+        y = randint(0, 250)
+        qp.drawEllipse(x, y, size, size)
+        qp.end()
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
+application = QApplication(sys.argv)
+sys.excepthook = except_hook
+executable = MainWindow()
+executable.show()
+sys.exit(application.exec_())
